10b9b1c12350da19eca0391798752c3706dc7a5e
Jake Helme <jake.helme@entelect.co.za>
2023-12-18 15:22:02 +0200

    Classic! Works on example for part 2, but not on my input. Moving computers

--- d19/index.js
+++ d19/index.js
@@ -17,6 +17,8 @@ hdj{m>838:A,pv}
 {x=2461,m=1339,a=466,s=291}
 {x=2127,m=1623,a=2188,s=1013}`;
 
+const input = await Bun.file('./d19/input.txt').text();
+
 const paramPos = { x: 0, m: 1, a: 2, s: 3 };
 
 function parseInput(raw) {
@@ -26,7 +28,12 @@ function parseInput(raw) {
 		const name = l.match(/^\w+/)[0];
 		const ruleArr = l.replace(/^\w+{/, '').replace(/}$/, '').split(',').map(r => {
 			const matches = r.match(/((\w+)([<>])(\d+):)?(\w+)/);
-			if (matches[1]) return { param: matches[2], isGT: matches[3] === '>', compare: Number(matches[4]), then: matches[5] };
+			if (matches[1]) return {
+				param: matches[2],
+				isGT: matches[3] === '>',
+				compare: Number(matches[4]),
+				then: matches[5]
+			};
 			return { then: matches[0] };
 		});
 		o[name] = ruleArr;
@@ -44,21 +51,22 @@ function processPart(workflows, part) {
 		for (const rule of workflows[wf]) {
 			if (rule.compare) {
 				if (rule.isGT) {
-					if(part[paramPos[rule.param] > rule.compare]) {
+					if (part[paramPos[rule.param]] > rule.compare) {
 						wf = rule.then;
 						break;
 					}
 				} else {
-					if(part[paramPos[rule.param] < rule.compare]) {
+					if (part[paramPos[rule.param]] < rule.compare) {
 						wf = rule.then;
 						break;
 					}
 				}
 			} else {
 				wf = rule.then;
+				break;
 			}
 		}
-		
+
 	}
 	return wf === 'A';
 }
@@ -66,9 +74,71 @@ function processPart(workflows, part) {
 function runPartsThroughWorkflows(workflows, parts) {
 	let tot = 0;
 	for (const part of parts) {
-		if(processPart(workflows, part)) tot += (part.reduce((t, c) => t + c));
+		if (processPart(workflows, part)) tot += (part.reduce((t, c) => t + c));
 	}
 	return tot;
 }
 
-console.log(runPartsThroughWorkflows(...parseInput(example)));
+// console.log(runPartsThroughWorkflows(...parseInput(example)));
+// console.log(runPartsThroughWorkflows(...parseInput(input)));
+
+function getDistinctRatingCombos(workflows) {
+	const validRanges = [];
+	for (const key of Object.keys(workflows)) {
+		const ranges = [[1, 4000], [1, 4000], [1, 4000], [1, 4000]];
+		let next = key;
+		let search = 'A';
+		while (true) {
+			let negate = false;
+			for (let i = workflows[next].length - 1; i >= 0; i--) {
+				const rule = workflows[next][i];
+				if (negate || rule.then === search) {
+					if (rule.compare) {
+						if (!negate) {
+							if (rule.isGT) {
+								ranges[paramPos[rule.param]][0] = Math.max(rule.compare + 1, ranges[paramPos[rule.param]][0]);
+							} else {
+								ranges[paramPos[rule.param]][1] = Math.min(rule.compare - 1, ranges[paramPos[rule.param]][1]);
+							}
+						} else {
+							if (rule.isGT) {
+								ranges[paramPos[rule.param]][1] = Math.min(rule.compare, ranges[paramPos[rule.param]][1]);
+							} else {
+								ranges[paramPos[rule.param]][0] = Math.max(rule.compare, ranges[paramPos[rule.param]][0]);
+							}
+						}
+
+					} else {
+						if (i >= 1 && rule.then === workflows[next][i - 1].then){
+							i--;
+						}
+					}
+					negate = true;
+				}
+			}
+			if (negate) {
+				if (next === 'in'){
+					validRanges.push(ranges);
+					break;
+				}
+				search: for (const searchKey in workflows) {
+					for (const searchRule of workflows[searchKey]) {
+						if (searchRule.then === next) {
+							search = next;
+							next = searchKey;
+							break search;
+						}
+					}
+				}
+			} else {
+				break;
+			}
+		}
+	}
+	// console.log();
+	return validRanges.reduce((total, range) => total + range.reduce((t, r) => t * (r[1] - r[0] + 1), 1), 0);
+}
+
+
+console.log(getDistinctRatingCombos(parseInput(example)[0]));
+console.log(getDistinctRatingCombos(parseInput(input)[0]));
