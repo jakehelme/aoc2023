536cd183f831ab30d0b0eccb8e4bf921949b379f
Jake Helme <jake.helme@entelect.co.za>
2023-12-20 15:02:35 +0200

    Moving

--- d20/index.js
+++ d20/index.js
@@ -1,21 +1,29 @@
 const example1 =
-`broadcaster -> a, b, c
+	`broadcaster -> a, b, c
 %a -> b
 %b -> c
 %c -> inv
 &inv -> a`;
 
 function pressButton(rules) {
+	const queue = [];
 
 }
 
 function parseInput(raw) {
-	raw.split('\n').reduce((m, l) => {
+	return raw.split('\n').reduce((m, l) => {
 		let line = l;
 		const obj = {};
-		if(!/\w/.test(line[0])) obj.type = line[0];
-		else 
-		
-		l.split(' -> ')
-	}, new Map())
+		if (!/\w/.test(line[0])) {
+			if(line[0] === '%')
+			obj.type = line[0];
+			line = line.substr(1);
+		}
+		const module = line.split(' -> ')[0];
+		obj.outputs = line.split(' -> ')[1].split(', ');
+		m.set(module, obj);
+		return m;
+	}, new Map());
 }
+
+console.log(pressButton(parseInput(example1)));
