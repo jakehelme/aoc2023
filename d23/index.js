f46495b906b9f6b8889f1082b215a898d4886f31
Jake Helme <jake.helme@entelect.co.za>
2023-12-20 17:01:58 +0200

    moving

--- d23/index.js
+++ d23/index.js
@@ -52,7 +52,7 @@ function findLongestPath(map, canClimbSlopes = false) {
 				const nTile = map[n[0]][n[1]];
 				if (n[0] === current[3] && n[1] === current[4]) return false;
 				if (nTile === '#') return false;
-				if(!canClimbSlopes) {
+				if (!canClimbSlopes) {
 					if (cTile === '.' && nTile === '.') return true;
 					if (cTile === '>' && i === 0) return true;
 					if (cTile === 'v' && i === 1) return true;
@@ -78,7 +78,7 @@ function findLongestPath(map, canClimbSlopes = false) {
 		if (neighbours.length > 1) {
 			for (const next of neighbours) {
 				const nextKey = genKey(next);
-				if (!current[5].has(nextKey)){
+				if (!current[5].has(nextKey)) {
 					const newVisited = new Set(current[5]);
 					newVisited.add(nextKey)
 					frontier.push([...next, current[2] + 1, ...current.slice(0, 2), newVisited]);
@@ -86,16 +86,59 @@ function findLongestPath(map, canClimbSlopes = false) {
 			}
 		} else if (neighbours.length === 1) {
 			const nextKey = genKey(neighbours[0]);
-				if (!current[5].has(nextKey)){
-					current[5].add(nextKey);
-					frontier.push([...neighbours[0], current[2] + 1, ...current.slice(0, 2), current[5]]);
-				}
+			if (!current[5].has(nextKey)) {
+				current[5].add(nextKey);
+				frontier.push([...neighbours[0], current[2] + 1, ...current.slice(0, 2), current[5]]);
+			}
 		}
 	}
 	return longest;
 }
 
+function buildGraphFromGrid(map) {
+	const distanceToNodes = {};
+	const nodes = {};
+	const start = [1, 1];
+	let last = [0, 1];
+	let lastNode = '0,1';
+	let dist = 1;
+	const frontier = [[...start, dist, ...last, lastNode]];
+	while (frontier.length) {
+		const current = frontier.shift();
+		const cTile = map[current[0]][current[1]];
+		const neighbours = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+			.map(d => d.map((p, i) => p + current[i]))
+			.filter(n => {
+				const nTile = map[n[0]][n[1]];
+				if (n[0] === current[3] && n[1] === current[4]) return false;
+				if (nTile === '#') return false;
+				if (/>|v|<|\^/.test(cTile)) return true;
+				if (cTile === '.' && nTile === '.') return true;
+				if (cTile === '.' &&
+					!(
+						dirMap[nTile][0] + n[0] === current[0] &&
+						dirMap[nTile][1] + n[1] === current[1]
+					)
+				) return true;
+				return false;
+			});
+
+			if (neighbours.length === 1) {
+				// last = [...current.slice(0, 2)];
+				// pos = [...neighbours[0]];
+				frontier.push([...neighbours[0], current[2] + 1, ...current.slice(0,2), current[5]]);
+			} else if (neighbours.length > 1) {
+				const key = `${lastNode}->${current[0]},${current[1]}`;
+				distanceToNodes[key] = dist;
+				nodes[key] = [];
+				lastNode = genKey(current);
+				console.log();
+			}
+	}
+}
+
 // console.log(findLongestPath(parseInput(example)));
 // console.log(findLongestPath(parseInput(input)));
 // console.log(findLongestPath(parseInput(example), true));
-console.log(findLongestPath(parseInput(input), true));
+// console.log(findLongestPath(parseInput(input), true));
+buildGraphFromGrid(parseInput(example));
