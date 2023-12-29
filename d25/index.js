834f46b63b8be24cbd3ce05ed27a8592631bf269
Jake Helme <jake.helme@entelect.co.za>
2023-12-29 10:25:33 +0200

    Day 25, just put it in a visualiser 🦥

--- d25/index.js
+++ d25/index.js
@@ -13,21 +13,71 @@ lsr: lhk
 rzs: qnr cmg lsr rsh
 frs: qnr lhk lsr`;
 
+const input = await Bun.file('./d25/input.txt').text();
+
 const parseInput = raw => raw.split('\n').reduce((m, line) => {
 	const modules = line.match(/\w+/g);
 	m.set(modules[0], new Set(modules.slice(1)));
 	return m;
 }, new Map());
 
-function blurple(nodes) {
-	const wires = new Set();
+function getUniqueGroups(nodes) {
+	const groups = [];
+	for (const node of nodes.keys()) {
+		const start = node;
+		const visited = new Set([start]);
+		const frontier = [start];
+		while(frontier.length) {
+			const current = frontier.shift();
+			for (const next of nodes.get(current)) {
+				if (!visited.has(next)) {
+					frontier.push(next);
+					visited.add(next);
+				}
+			}
+		}
+		if (visited.size === nodes.size) {
+			groups.push(visited);
+			return groups;
+		} else {
+			if (groups.length) {
+				let isUnique = true;
+				for (const group of groups) {
+					if (visited.difference(group).size === 0) {
+						isUnique = false;
+						break;
+					}
+				}
+				if (isUnique) groups.push(visited);
+			} else {
+				groups.push(visited);
+			}
+		}
+	}
+	return groups;
+}
+
+async function blurple(nodes) {
+	// const wires = new Set();
+	const wires = [];
 	for (const [node, otherNodes] of nodes) {
 		for (const otherNode of otherNodes) {
 			const wire = [node, otherNode];
 			wire.sort();
-			wires.add(wire.join('-'));
+			// wires.add(wire.join('-'));
+			wires.push(wire.join('-'));
 		}
 	}
+
+	// create viz
+	// const Links = [["Source", "Target"]]
+	// for (const [node, others] of nodes) {
+	// 	for (const other of others) {
+	// 		Links.push([node, other]);
+	// 	}
+	// }
+
+	// await Bun.write('./nodes.json', JSON.stringify(Links));
 
 	for (const [node, otherNodes] of nodes) {
 		for (const otherNode of otherNodes) {
@@ -36,7 +86,39 @@ function blurple(nodes) {
 		}
 	}
 
-	console.log();
+	// for (let i = 0; i < wires.length - 2; i++) {
+	// 	console.log(`${i + 1}/${wires.length - 2}`);
+	// 	for (let j = i + 1; j < wires.length - 1; j++) {
+	// 		for (let k = j + 1; k < wires.length; k++) {
+	// 			const [a, b] = wires[i].split('-');
+	// 			const [c, d] = wires[j].split('-');
+	// 			const [e, f] = wires[k].split('-');
+	// 			const tempNodes = structuredClone(nodes);
+	// 			tempNodes.get(a).delete(b);
+	// 			tempNodes.get(b).delete(a);
+	// 			tempNodes.get(c).delete(d);
+	// 			tempNodes.get(d).delete(c);
+	// 			tempNodes.get(e).delete(f);
+	// 			tempNodes.get(f).delete(e);
+	// 			const uniqueGroups = getUniqueGroups(tempNodes);
+	// 			if (uniqueGroups.length === 2) {
+	// 				return uniqueGroups[0].size * uniqueGroups[1].size;
+	// 			};
+	// 		}
+	// 	}
+	// }
+	const tempNodes = structuredClone(nodes);
+	tempNodes.get('jkn').delete('cfn');
+	tempNodes.get('cfn').delete('jkn');
+	tempNodes.get('sfd').delete('ljm');
+	tempNodes.get('ljm').delete('sfd');
+	tempNodes.get('rph').delete('gst');
+	tempNodes.get('gst').delete('rph');
+	const uniqueGroups = getUniqueGroups(tempNodes);
+	return uniqueGroups[0].size * uniqueGroups[1].size;
+	// return null;
 }
 
-console.log(blurple(parseInput(example)));
+// console.log(blurple(parseInput(example)));
+const ans = await blurple(parseInput(input));
+console.log(ans);
